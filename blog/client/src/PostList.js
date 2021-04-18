6d3d2b5ffdae5e33b795b6e3bcb6135d6d179325
Jisu <jisuc@credfin.io>
2021-04-18 14:29:34 +1000

    Added moderation service and updated query service

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import CommentCreate from './CommentCreate';
+import CommentList from './CommentList'
 
 export default () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
+    const res = await axios.get("http://localhost:4002/posts");
 
     setPosts(res.data);
   };
@@ -23,6 +25,8 @@ export default () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList comments={post.comments}/>
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
